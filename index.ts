6738d7facc70cf99a6deb07fe6f5200fe6dedd4e
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2026-01-31 23:27:57 +0000

    Setup agent

---
+++ index.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import 'dotenv/config';
+import { generateText } from 'ai';
+import { huggingface } from '@ai-sdk/huggingface';
+import { ollama } from 'ai-sdk-ollama';
+import { SYSTEM_PROMPT } from './system-prompt.js';
+// import { tools } from './tools/index.ts';
+// import { executeTool } from './executeTools.ts';
+
+export async function runAgent(prompt: string) {
+  // const { text, toolCalls } = await generateText({
+  //   model: huggingface('deepseek-ai/DeepSeek-V3-0324'),
+  //   prompt,
+  //   system: SYSTEM_PROMPT,
+  //   tools,
+  // });
+
+  const { text } = await generateText({
+    model: ollama('gpt-oss:120b-cloud'),
+    prompt,
+    system: SYSTEM_PROMPT,
+    providerOptions: { ollama: { think: true } },
+    // tools,
+  });
+
+  console.log(text);
+
+  // toolCalls?.forEach(async (call) => {
+  //   const toolResult = await executeTool(call.toolName, call.input);
+  //   console.log(`Tool "${call.toolName}" executed with result:`, toolResult);
+  // });
+}
+
+runAgent('Tell me a joke about computers.');
